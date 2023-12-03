864505804d176ae3ad344e6855a836225ebc497e
Sugapriyan-P-K <sugapriyan2003@gmail.com>
2023-12-03 19:40:59 +0530

    first program

---
+++ evenOddPattern.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void drawPattern(int n) {
+    int even = 2, odd = 1, row = 1, idx = 0;
+    while (row <= n) {
+        idx = 0;
+        while (idx < row && row <= n) {
+            printf("%d ", odd);
+            odd += 2;
+            idx++;
+        }
+        row++;
+        idx = 0;
+        printf("\n");
+        while (idx < row && row <= n) {
+            printf("%d ", even);
+            idx++;
+            even += 2;
+        }
+        row++;
+        printf("\n");
+    }
+}
+
+int main(void) {
+    int n;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    drawPattern(n);
+}
