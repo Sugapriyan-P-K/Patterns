d8af8f0bf157e5d643d6533333fd7491e5090363
Sugapriyan-P-K <sugapriyan2003@gmail.com>
2024-02-21 20:07:36 +0530

    modified

--- pattern.java
+++ pattern.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
 public class pattern {
+    public static void anotherMethod(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + (n - 2 * i); j++) {
+                System.out.print(" ");
+            }
+            for (int l = i; l > 0; l--) {
+                System.out.print(l + " ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void printPattern(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n + (n - 2 * i); j++) {
@@ -26,6 +41,7 @@ public class pattern {
             System.out.println("Invalid input");
         }
         printPattern(n);
+        anotherMethod(n); // from start of 0
         scan.close();
     }
 }
