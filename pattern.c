3d512e0493d4d087405d13eae0f26a072ff83f86
Sugapriyan-P-K <sugapriyan2003@gmail.com>
2024-02-21 20:11:06 +0530

    modified

--- pattern.c
+++ pattern.c
@@ -1,6 +1,21 @@
 #include "patternHeader.h"
 
 
+void method2() {
+    int n = getInput();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n + (n - 2 * i); j++) {
+            printf(" ");
+        }
+        for (int l = i; l > 0; l--) {
+            printf("%d ", l);
+        }
+        for (int k = 0; k <= i; k++) {
+            printf("%d ", k);
+        }
+        printf("\n");
+    }
+}
 void drawPattern() {
     int n = getInput();
     for (int i = 0; i < n; i++) {
@@ -20,4 +35,5 @@ void drawPattern() {
 
 int main(void) {
     drawPattern();
+    method2();
 }
