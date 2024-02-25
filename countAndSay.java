4af2557077c71e2049d15b914051ce7133066a7c
Sugapriyan-P-K <sugapriyan2003@gmail.com>
2024-02-25 20:39:32 +0530

    count and say

---
+++ countAndSay.java
@@ -0,0 +1,32 @@
+public class countAndSay {
+    public static String CountAndSay(int n) {
+        String s = "1";
+        for (int i = 1; i < n; i++) {
+            s = countIdx(s);
+        }
+        return s;
+    }
+
+    public static String countIdx(String s) {
+        String str = "";
+        char c = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                count++;
+            } else {
+                str += (char) (count + '0');
+                str += c;
+                c = s.charAt(i);
+                count = 1;
+            }
+        }
+        str += (char) (count + '0');
+        str += c;
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CountAndSay(5));
+    }
+}
